88ee62c11bc828fcef4c4f21c1573114c4d1a5b3
Charlie Field <charlie.field98@gmail.com>
2024-07-02 14:54:59 +0100

    Added friend table sql script and to db_entities.

---
+++ api.common/database/create_friend_table.sql
@@ -0,0 +1,51 @@
+-- Create friend status + friend tables.
+DELIMITER $$
+
+DROP PROCEDURE IF EXISTS `loop`.`temp_migration_function` $$
+CREATE PROCEDURE `loop`.`temp_migration_function`()
+BEGIN
+
+IF (SELECT TABLE_NAME FROM information_schema.tables WHERE table_schema = 'loop' AND table_name = 'friend_status') IS NULL THEN
+    CREATE TABLE `friend_status` (
+        `id` INT NOT NULL AUTO_INCREMENT,
+        `description` VARCHAR(255) NULL,
+    PRIMARY KEY (`id`)
+    );
+
+END IF;
+
+INSERT INTO `loop`.`friend_status` (description)
+SELECT 'Friends'
+WHERE NOT EXISTS (SELECT 1 FROM `loop`.`friend_status` WHERE description = 'Friends');
+
+INSERT INTO `loop`.`friend_status` (description)
+SELECT 'Pending'
+WHERE NOT EXISTS (SELECT 1 FROM `loop`.`friend_status` WHERE description = 'Pending');
+
+INSERT INTO `loop`.`friend_status` (description)
+SELECT 'Blocked'
+WHERE NOT EXISTS (SELECT 1 FROM `loop`.`friend_status` WHERE description = 'Blocked');
+
+
+IF (SELECT TABLE_NAME FROM information_schema.tables WHERE table_schema = 'loop' AND table_name = 'friend') IS NULL THEN
+    CREATE TABLE `friend` (
+        `id` INT NOT NULL AUTO_INCREMENT,
+        `friend_1` INT NOT NULL,
+        `friend_2` INT NOT NULL,
+        `status` INT NOT NULL,
+        FOREIGN KEY (`friend_1`) REFERENCES `user`(`id`),
+        FOREIGN KEY (`friend_2`) REFERENCES `user`(`id`),
+        FOREIGN KEY (`status`) REFERENCES `friend_status`(`id`),
+        PRIMARY KEY (`id`)
+    );
+ALTER TABLE `friend` ADD UNIQUE unique_model_signal(
+        `friend_1`, `friend_2`
+);
+END IF;
+
+END $$
+
+CALL `loop`.`temp_migration_function`() $$
+DROP PROCEDURE `loop`.`temp_migration_function` $$
+
+DELIMITER ;
--- api.common/loop/db_entities.py
+++ api.common/loop/db_entities.py
@@ -24,6 +24,8 @@ def define_entities(db: Database):
         first_name = Required(str)
         last_name = Required(str)
         ratings = Set('Rating')
+        friend_1 = Set('Friend', reverse='friend_1')
+        friend_2 = Set('Friend', reverse='friend_2')
 
     class Location(db.Entity):
         """
@@ -45,5 +47,18 @@ def define_entities(db: Database):
         food = Required(int)
         location = Required(Location)
         user = Required(User)
+        created = Optional(datetime)
+        last_updated = Optional(datetime)
+
+    class Friend_status(db.Entity):
+        id = PrimaryKey(int, auto=True)
+        description = Required(str)
+        status = Set('Friend')
+
+    class Friend(db.Entity):
+        id = PrimaryKey(int, auto=True)
+        friend_1 = Required(User, reverse='friend_1')
+        friend_2 = Required(User, reverse='friend_2')
+        status = Required(Friend_status)
         created = Optional(datetime)
         last_updated = Optional(datetime)
