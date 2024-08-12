859109efa670e669d9a394a700e18c7f1551d02a
Charlie Field <charlie.field98@gmail.com>
2024-08-12 14:00:03 +0100

    Fixed test google places.

--- api.common/loop/tests/test_google_places.py
+++ api.common/loop/tests/test_google_places.py
@@ -3,14 +3,19 @@ from unittest.mock import Mock, call, patch
 
 import googlemaps
 from googlemaps.exceptions import ApiError
+from loop.api_classes import Coordinates
 from loop.exceptions import GoogleApiError
 from loop.google_client import GooglePlaces
 from loop.utils import Location
 
+TEST_COORDINATES = Coordinates(lat=1.0, lng=1.0)
+
 
 class TestGooglePlaces(unittest.TestCase):
+    @patch('loop.google_client.places.get_secret')
     @patch.object(googlemaps, 'Client')
-    def setUp(self, mock_googlemaps):
+    def setUp(self, mock_googlemaps, mock_secret):
+        mock_secret.return_value = {'key': 'mock_secret'}
         self.mock_googlemaps = mock_googlemaps
         self.google_places = GooglePlaces()
 
@@ -144,44 +149,53 @@ class TestGooglePlaces(unittest.TestCase):
         )
 
     def test_search(self):
-        place_response = {
-            'status': 'OK',
-            'result': {
-                'formatted_address': 'Test Address, Test City',
-                'name': 'Test Name',
-            },
-        }
+        candidates = [
+            {
+                'formatted_address': 'Saint Katherine Docks, E Smithfield, London E1W 1AT, United Kingdom',
+                'geometry': {
+                    'location': {
+                        'lat': 51.5074562,
+                        'lng': -0.07120989999999999,
+                    },
+                    'viewport': {
+                        'northeast': {
+                            'lat': 51.50917822989272,
+                            'lng': -0.06975287010727778,
+                        },
+                        'southwest': {
+                            'lat': 51.50647857010728,
+                            'lng': -0.07245252989272222,
+                        },
+                    },
+                },
+                'name': 'Bravas Tapas',
+            }
+        ]
         search_response = {
+            'candidates': candidates,
             'status': 'OK',
-            'candidates': [{'place_id': 'test_place_id'}],
         }
         self.mock_googlemaps.return_value.find_place.return_value = (
             search_response
         )
-        self.mock_googlemaps.return_value.place.return_value = place_response
         text_search = 'Test search'
-        locations = self.google_places.search(text_search)
-        self.assertEqual(
-            locations,
-            [
-                Location(
-                    google_id='test_place_id',
-                    address='Test Address, Test City',
-                    display_name='Test Name',
-                )
-            ],
-        )
+        locations = self.google_places.search(text_search, TEST_COORDINATES)
+        self.assertEqual(locations, candidates)
 
     def test_search_type_error(self):
         text_search = ['Test search']
-        self.assertRaises(TypeError, self.google_places.search, text_search)
+        self.assertRaises(
+            TypeError, self.google_places.search, text_search, TEST_COORDINATES
+        )
 
     def test_search_api_error(self):
         self.mock_googlemaps.return_value.find_place.side_effect = ApiError(
             500
         )
         text_search = 'Test search'
-        self.assertRaises(ApiError, self.google_places.search, text_search)
+        self.assertRaises(
+            ApiError, self.google_places.search, text_search, TEST_COORDINATES
+        )
 
 
 if __name__ == '__main__':
