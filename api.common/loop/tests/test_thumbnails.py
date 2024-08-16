e690bd859e7c1cbd443dbb7b3a80ce4088b67b72
Charlie Field <charlie.field98@gmail.com>
2024-08-16 19:34:55 +0100

    Increased coverage in thumbnails.py.

--- api.common/loop/tests/test_thumbnails.py
+++ api.common/loop/tests/test_thumbnails.py
@@ -2,7 +2,11 @@ import unittest
 from unittest.mock import Mock, call, patch
 
 from loop.data_classes import UploadThumbnailEvent
-from loop.thumbnails import ThumbnailUploader
+from loop.thumbnails import (
+    RestaurantThumbnails,
+    ThumbnailUploader,
+    upload_thumbnail,
+)
 
 TEST_PLACE_ID = 'test_place_id'
 TEST_PHOTO_REFERENCE = 'test_photo_reference'
@@ -49,6 +53,65 @@ class TestThumbnailUploader(unittest.TestCase):
         self.assertRaises(
             TypeError, self.uploader.upload_thumbnail, error_event
         )
+
+
+class TestRestaurantThumbnails(unittest.TestCase):
+    @patch('loop.thumbnails.S3Service')
+    def setUp(self, mock_s3_service):
+        self.mock_s3_service = mock_s3_service
+        self.thumbnails = RestaurantThumbnails()
+
+    def tearDown(self):
+        self.mock_s3_service = None
+        self.thumbnails = None
+
+    def test_check_item_exists(self):
+        place_id = 'test_place_id'
+        self.thumbnails.check_item_exists(place_id)
+        # Check mocks
+        self.assertEqual(
+            self.mock_s3_service.mock_calls[0],
+            call('loop-s3-restaurant-thumbnail-store-test'),
+        )
+        self.assertEqual(
+            self.mock_s3_service.mock_calls[1],
+            call().item_exists('test_place_id.jpeg'),
+        )
+
+
+class TestUploadThumbnail(unittest.TestCase):
+    @patch('loop.thumbnails.SqsClient')
+    def test_upload_thumbnail(self, mock_sqs_client):
+        upload_thumbnail(
+            UploadThumbnailEvent(
+                place_id='test_place_id',
+                photo_reference='some_photo_reference',
+            )
+        )
+        self.assertEqual(
+            mock_sqs_client.mock_calls,
+            [
+                call('loop-sqs-restaurant-thumbnail-test'),
+                call().send_message(
+                    {
+                        'place_id': 'test_place_id',
+                        'photo_reference': 'some_photo_reference',
+                    }
+                ),
+            ],
+        )
+
+    @patch('loop.thumbnails.SqsClient')
+    def test_upload_thumbnail_type_error(self, mock_sqs_client):
+        self.assertRaises(
+            TypeError,
+            upload_thumbnail,
+            {
+                'place_id': 'test_place_id',
+                'photo_reference': 'some_photo_reference',
+            },
+        )
+        self.assertFalse(mock_sqs_client.called)
 
 
 if __name__ == '__main__':
